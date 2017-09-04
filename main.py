efa508d2ef0815e9a38d06b5de578a781e2fc7d7
Heather Lent <hclent@email.arizona.edu>
2017-09-04 13:39:58 -0700

    Updating main

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import logging, os, sys
 from readabstracttxt import retrieve_text_files
-from readpatentpdf import convertPdfs
 scriptpath = os.path.abspath(os.path.join(os.path.dirname(__file__), 'train'))
 sys.path.append(scriptpath)
 from abstract2vec import train_d2v
@@ -10,22 +9,17 @@ logging.basicConfig(level=logging.DEBUG,
                     format='%(asctime)s %(levelname)-8s %(message)s',
                     datefmt='%a, %d %b %Y %H:%M:%S',
                     filename='.pypatent.log')
-#Step 1: Convert pdf patents to txt, save in /train
-#Step 2: Extract abstracts, then save as txt files in /train
-#Step 3: If there is not a trained model, train the doc2vec model. Otherwise, load the doc2vec model
-#Step 4: Create csv output document with patent-abstract comparisons  
+#Step 1: Extract abstracts, then save as txt files in /train
+#Step 2: If there is not a trained model, train the doc2vec model. Otherwise, load the doc2vec model
+#Step 3: Create csv output document with patent-abstract comparisons  
 
 #Run project
 def run_pypatent():
-	#Step 1: Convert patents to .txts 
-	logging.info("* BEGIN CONVERTING PATENT PDF'S ... ")
-	convertPdfs()
-	logging.info("* FINISHED CONVERTING PATENT PDF'S !!!")
-	#Step 2: Extract abstracts 
+	#Step 1: Extract abstracts from literature searches 
 	logging.info("* BEGIN RETRIEVING ABSTRACTS ... ")
 	retrieve_text_files()
 	logging.info("* FINISHED RETRIEVING ABSTRACTS !!! ")
-	# Step 3: Train doc2vec 
+	#Step 2: Train doc2vec 
 	path_to_model = os.path.abspath(os.path.join(os.path.dirname(__file__), 'train', './a2v.d2v'))
 	if os.path.isfile(path_to_model):
 		logging.info("* A TRAINED MODEL ALREADY EXISTS !!!")
@@ -34,7 +28,7 @@ def run_pypatent():
 		logging.info("* TRAINED MODEL NOT FOUND. TRAINING MODEL ... ")
 		train_d2v()
 		logging.info("* FINISHED TRAINING MODEL !!! ")
-	# Step 4: Get results
+	# Step 3: Get results
 
 
 
